1f52961ee1ccf734510ab8e06f85a8fdff85324c
xdutsuay <kaustubh.best01@gmail.com>
2024-03-03 23:31:52 +0530

    Persistent seems to lost, tried login need to configure firebase. later now()

--- lib/main.dart
+++ lib/main.dart
@@ -4,8 +4,12 @@ import 'persi.dart';
 import 'sidedrawer.dart';
 import 'package:intl/intl.dart'; // Import the intl package to format dates
 import  'methods.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'login.dart';
 
-void main() {
+void main() async {
+  // WidgetsFlutterBinding.ensureInitialized();
+  // await Firebase.initializeApp(); // later when firebase is configured
   runApp(MyApp()); 
 }
 class MyApp extends StatelessWidget {
